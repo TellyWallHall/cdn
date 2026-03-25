9cd9a25da48e0e18a09fb188edc4e3420d6e5309
TellyWallHall <littlenibbscabinboyforever@gmail.com>
2026-03-24 21:10:08 -0400

    Delete scripts/index.js

--- scripts/index.js
+++
@@ -1,9 +0,0 @@
-console.clear();
-  console.time('Test');
-  console.log('Hello World!');
-  console.error('Oh no World! Something went wrong');
-  console.warn('Hey World, be careful!');
-  console.info('Psst, World, here\'s some info');
-  console.table([{ message: 'Hello Table' }, { message: 'It\'s a Table' }]);
-  console.timeEnd('Test');
-  console.assert(false, 'Probably fine');
